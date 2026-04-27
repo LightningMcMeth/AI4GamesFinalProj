9252548bcc94e3b50370f68ce5e8b69e921701c6
YuriiSushko <ura12334567@gmail.com>
2026-04-27 13:42:09 +0300

    [assets] added some assets + enhanced ui

--- proj/Assets/Scripts/Game/Unity/CellView.cs
+++ proj/Assets/Scripts/Game/Unity/CellView.cs
@@ -1,5 +1,4 @@
 #if UNITY_EDITOR
-using UnityEditor;
 #endif
 
 using System.Collections.Generic;
@@ -44,7 +43,7 @@ namespace AI4GamesFinalProj.Gameplay
             if (forceVisualRebuild || currentVisual == null || currentVisualType != cell.Type)
             {
                 RebuildVisual(cell.Type);
-                SetEditorIcon();
+                //SetEditorIcon();
             }
 
             UpdateFallbackTint();
@@ -189,28 +188,28 @@ namespace AI4GamesFinalProj.Gameplay
             statusText.fontSize = 32;
         }
 
-#if UNITY_EDITOR
-        private void SetEditorIcon()
-        {
-            if (currentCell == null)
-                return;
+        //#if UNITY_EDITOR
+        //        private void SetEditorIcon()
+        //        {
+        //            if (currentCell == null)
+        //                return;
 
-            Texture2D icon = currentCell.Type switch
-            {
-                CellType.HealthyLand => EditorGUIUtility.IconContent("sv_label_3").image as Texture2D,   // green
-                CellType.CorruptedLand => EditorGUIUtility.IconContent("sv_label_6").image as Texture2D, // red
-                CellType.ManaSpring => EditorGUIUtility.IconContent("sv_label_1").image as Texture2D,    // blue
-                CellType.LifeRoot => EditorGUIUtility.IconContent("sv_label_2").image as Texture2D,      // teal
-                CellType.SacredCell => EditorGUIUtility.IconContent("sv_label_4").image as Texture2D,    // yellow
-                CellType.DeadCell => EditorGUIUtility.IconContent("sv_label_0").image as Texture2D,      // gray
-                _ => null
-            };
+        //            Texture2D icon = currentCell.Type switch
+        //            {
+        //                CellType.HealthyLand => EditorGUIUtility.IconContent("sv_label_3").image as Texture2D,   // green
+        //                CellType.CorruptedLand => EditorGUIUtility.IconContent("sv_label_6").image as Texture2D, // red
+        //                CellType.ManaSpring => EditorGUIUtility.IconContent("sv_label_1").image as Texture2D,    // blue
+        //                CellType.LifeRoot => EditorGUIUtility.IconContent("sv_label_2").image as Texture2D,      // teal
+        //                CellType.SacredCell => EditorGUIUtility.IconContent("sv_label_4").image as Texture2D,    // yellow
+        //                CellType.DeadCell => EditorGUIUtility.IconContent("sv_label_0").image as Texture2D,      // gray
+        //                _ => null
+        //            };
 
-            if (icon != null)
-            {
-                EditorGUIUtility.SetIconForObject(gameObject, icon);
-            }
-        }
-#endif
+        //            if (icon != null)
+        //            {
+        //                EditorGUIUtility.SetIconForObject(gameObject, icon);
+        //            }
+        //        }
+        //#endif
     }
 }
